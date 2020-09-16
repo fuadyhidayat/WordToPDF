74296b104d2d8fde2566256b1963259775b47629
Fuady Rosma Hidayat <33979757+fuadyhidayat@users.noreply.github.com>
2020-09-16 16:59:16 +0700

    Script for converting Word files to PDF
    Diminta oleh Gita ;)

---
+++ WordToPDF.ps1
@@ -0,0 +1,24 @@
+$documents_path = '.'
+
+$word_app = New-Object -ComObject Word.Application
+
+# This filter will find .doc as well as .docx documents
+Get-ChildItem -Path $documents_path -Filter *.doc? | ForEach-Object {
+
+    $document = $word_app.Documents.Open($_.FullName)
+
+    $pdf_filename = "$($_.DirectoryName)\$($_.BaseName).pdf"
+
+    $document.SaveAs([ref] $pdf_filename, [ref] 17)
+
+    $document.Close()
+}
+
+#Close Word
+[gc]::Collect()
+[gc]::WaitForPendingFinalizers()
+$word_app.Quit()
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($word_app)
+
+#Cleanup
+Remove-Variable word_app
